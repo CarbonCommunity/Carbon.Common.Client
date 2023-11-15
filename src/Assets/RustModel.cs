85a0c19828c7ace0913c48aa3d13e635e6168672
raul <raulssorban@gmail.com>
2023-11-15 22:32:47 +0100

    Added RustModel

---
+++ src/Assets/RustModel.cs
@@ -0,0 +1,18 @@
+﻿using ProtoBuf;
+using UnityEngine;
+
+namespace Carbon.Client
+{
+	[ProtoContract]
+	public partial class RustModel : MonoBehaviour
+	{
+		#region Editor
+
+		public GameObject Prefab;
+
+		#endregion
+
+		[ProtoMember(1)]
+		public string PrefabPath { get; set; }
+	}
+}
