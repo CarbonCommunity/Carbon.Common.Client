adc1a57333c658b704703d9fa128dc35995413b9
raul <raulssorban@gmail.com>
2023-12-11 00:58:36 +0100

    Client ready

---
+++ src/Patches/ServerMgr_ClientReady.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Carbon.Client;
+using Carbon.Extensions;
+using HarmonyLib;
+using JetBrains.Annotations;
+using Network;
+using UnityEngine;
+
+namespace Carbon.Common.Client.Patches;
+
+/*
+ *
+ * Copyright (c) 2022-2023 Carbon Community
+ * All rights reserved.
+ *
+ */
+
+[HarmonyPatch(typeof(ServerMgr), nameof(ServerMgr.ClientReady), new Type[] { typeof(Message) })]
+[UsedImplicitly]
+public class ServerMgr_ClientReady
+{
+	public static void Prefix(Message packet)
+	{
+		// OnCarbonClientReady
+		HookCaller.CallStaticHook(4004023948, packet.connection.ToCarbonClient());
+	}
+}
